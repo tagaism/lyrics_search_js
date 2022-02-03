c2e11d72fa739db8a63e0787098f7418a8953549
Adilet Nasirov <adilet.nasirov@yahoo.com>
2022-02-02 16:32:26 -0800

    decided to show on modal window

--- onClick.js
+++ onClick.js
@@ -2,12 +2,6 @@ const btn = document.querySelector(".get-lyrics");
 
 export function onClick(data, id) {
   console.log(data);
-  // console.log(id);
   const song = data.filter((el) => el.id === Number(id));
-  //   for (let el of data) {
-  //     if (el.id === Number(id)) {
-  //       console.log(el);
-  //     }
-  //   }
   console.log(song[0].artist);
 }
